82ab3b5efaf740b5ef87617e1b1613be4a7f4934
Raisul Islam <ris@fiftytwodigital.com>
2025-04-24 19:17:31 +0600

    requirement100

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,3 @@
-# SQLite patch for ChromaDB compatibility on Streamlit Cloud
-import os
-import sys
-
-# Check if running on Streamlit Cloud
-if os.environ.get('STREAMLIT_SHARING_MODE') == 'streamlit':
-    # Try to use pysqlite3 if available
-    try:
-        __import__('pysqlite3')
-        sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-    except ImportError:
-        # If pysqlite3 is not available, continue with system sqlite3
-        pass
 
 import streamlit as st
 import ollama
